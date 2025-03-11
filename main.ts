66388212a51ff79ff84816c9b4465849dc3aa2b4
paycheckpacheck <140981896+paycheckpacheck@users.noreply.github.com>
2025-03-11 14:52:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+radio.onReceivedString(function (receivedString) {
+    if ("youre infected" == receivedString) {
+        radio.setTransmitPower(0.05)
+        while (true) {
+            basic.showIcon(IconNames.Ghost)
+            radio.sendString(receivedString)
+            control.waitMicros(1000)
+        }
+    } else if ("im alive" == receivedString) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+    	
+    }
+})
+let Infected = 0
+if (randint(0, 5) == randint(0, 5)) {
+    basic.showIcon(IconNames.Ghost)
+    Infected = 1
+} else {
+    Infected = 0
+}
+radio.setTransmitPower(0.05)
+basic.forever(function () {
+    if (Infected == 1) {
+        basic.showIcon(IconNames.Ghost)
+        radio.sendString("youre infected")
+        control.waitMicros(4000)
+    } else {
+        basic.showIcon(IconNames.Heart)
+        radio.setTransmitPower(2)
+        radio.sendString("im alive")
+        radio.setTransmitPower(0.05)
+        control.waitMicros(4000)
+    }
+})
